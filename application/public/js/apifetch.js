fa5d8a8e947c3a565007a2a3a1c0537680b6a6df
ethanic17 <ethanzhengmain12@gmail.com>
2023-07-12 15:46:24 -0700

    css flex containers base

--- application/public/js/apifetch.js
+++ application/public/js/apifetch.js
@@ -2,4 +2,9 @@ async function logPhotos() {
     const response = await fetch("https://jsonplaceholder.typicode.com/albums/2/photos");
     const photos = await response.json();
     console.log(photos);
-  }
+}
+
+
+
+
+logPhotos();
