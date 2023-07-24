e960e89677432e737d86e3c86a27b782a9a4e7d3
ethanic17 <ethanzhengmain12@gmail.com>
2023-07-24 13:26:34 -0700

    index.js registration get request

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:"Ethan Zheng" });
 });
 
+router.get('/', function(req, res, next) {
+  res.render('registration', { title: 'CSC 317 App', name:"Ethan Zheng" });
+});
+
 module.exports = router;
