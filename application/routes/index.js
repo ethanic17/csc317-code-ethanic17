a611641bca09b6cb88b0db63b1f8f063491a940a
ethanic17 <ethanzhengmain12@gmail.com>
2023-07-24 13:54:22 -0700

    applied header partial to all webpages

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,8 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 317 App', name:"Ethan Zheng" });
+  res.render('index', { 
+    title: 'CSC 317 App', 
+    name:"Ethan Zheng" });
+
+  // let cssPath = path.resolve(css, "public");
+
+  // index.use(express.static(cssPath));
 });
+
+router.get('/registration', function(req, res, next) {
+  res.render('registration', { title: 'CSC 317 App', name:"Ethan Zheng" });
+});
+
+router.get('/postvideo', function(req, res, next) {
+  res.render('postvideo', { title: 'CSC 317 App', name:"Ethan Zheng" });
+});
+
+router.get('/viewpost', function(req, res, next) {
+  res.render('viewpost', { title: 'CSC 317 App', name:"Ethan Zheng" });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'CSC 317 App', name:"Ethan Zheng" });
+});
+
+
 
 
 
