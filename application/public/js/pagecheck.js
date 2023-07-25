7912f4da62245f28866cabc5ff824a997c565e80
ethanic17 <ethanzhengmain12@gmail.com>
2023-07-25 13:43:56 -0700

    navbar changing active class complete

--- application/public/js/pagecheck.js
+++ application/public/js/pagecheck.js
@@ -1,7 +1,29 @@
+// javascript function to check what page the user is on, and then add active class to navbar
+
 function pageCheck() {
     if (document.location.pathname === "/") {
         var home = document.getElementById("home");
         home.classList.add("active"); 
+    }
+
+    if (document.location.pathname === "/login") {
+        var login = document.getElementById("login");
+        login.classList.add("active"); 
+    }
+
+    if (document.location.pathname === "/registration") {
+        var registration = document.getElementById("register");
+        registration.classList.add("active"); 
+    }
+
+    if (document.location.pathname === "/postvideo") {
+        var postvideo = document.getElementById("postvideo");
+        postvideo.classList.add("active"); 
+    }
+
+    if (document.location.pathname === "/viewpost") {
+        var viewpost = document.getElementById("viewpost");
+        viewpost.classList.add("active"); 
     }
 }
 
